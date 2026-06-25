17cb26bb717a88d74f6d6a1cd6af583a15f5b46d
sarumon9 <60652742+sarumon9@users.noreply.github.com>
2026-06-25 11:21:09 +0900

    Add files via upload
    JSのコード記述開始

---
+++ JSColor/main.js
@@ -0,0 +1,11 @@
+
+const start = document.getElementById("defo");
+const end = document.getElementById("change");
+let btn = document.getElementById("btn");
+
+let show = document.getElementById("show");
+let show1 = document.getElementById("show1");
+
+btn.addEventListener(click, function(){
+    show1.style.backgroundColor = "#red";
+})
